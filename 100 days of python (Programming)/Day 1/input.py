736002552c0d8d9b45a4468933b7e04a173416bb
Satyam Kumar <75772136+Satyam2003-dev@users.noreply.github.com>
2023-12-08 17:22:02 +0530

    Update input.py

--- 100 days of python (Programming)/Day 1/input.py
+++ 100 days of python (Programming)/Day 1/input.py
@@ -3,3 +3,17 @@ print("Hello " + input("What is your Name?"))
 print(input)
 
 # input function is used to take input from user
+
+# # Fix the code below 👇
+
+# print(Day 1 - String Manipulation")
+# print("String Concatenation is done with the "+" sign.")
+#   print('e.g. print("Hello " + "world")')
+# print(("New lines can be created with a backslash and n.")
+
+#  Solution 👇
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
