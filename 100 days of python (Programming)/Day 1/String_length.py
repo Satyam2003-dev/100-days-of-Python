20bc0779959e1aa7b46620efc9f9ea5f934b1e88
Satyam Kumar <75772136+Satyam2003-dev@users.noreply.github.com>
2023-12-08 17:19:32 +0530

    Update String_length.py

--- 100 days of python (Programming)/Day 1/String_length.py
+++ 100 days of python (Programming)/Day 1/String_length.py
@@ -1,2 +1,7 @@
 # len function is used to count length of words
-print(len(input("What is your Name")))
+# print(len(input("What is your Name")))
+
+
+name = input("enter your name: ")
+print(name)
+print(len(name))
