8edc3e24865e4f1ddbc24fa59814a9a20bcf5df0
Satyam Kumar <75772136+Satyam2003-dev@users.noreply.github.com>
2023-12-08 17:18:00 +0530

    Add another code

--- 100 days of python (Programming)/Day 1/Day 1 Project/Band_Name_Generator.py
+++ 100 days of python (Programming)/Day 1/Day 1 Project/Band_Name_Generator.py
@@ -6,3 +6,12 @@ pet = input("What's your pet name ")
 # print("Your Pet name is" + " " + pet)
 print("Your band name could be")
 print(name + " " + pet)
+
+# Another way 
+
+print("Welcome to the Band Name Generator")
+
+name1 = input("What is your favorite color: ")
+name2 = input("Enter another name like bay, band or something you like: ")
+
+print("your band name is: " + name1 + " " + name2)
